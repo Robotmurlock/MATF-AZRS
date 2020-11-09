e441e2665cd3e617bfcd1f1b854edc8481a9238f
Robotmurlock <momir.adzemovic@gmail.com>
2020-11-09 13:13:00 +0100

    Ispravljen 04 primer za gdb

--- tema04_gdb/04_factorial/main.cpp
+++ tema04_gdb/04_factorial/main.cpp
@@ -5,13 +5,12 @@ int a = 3, b = 5, c = 7;
 int factorial(int n)
 {
     int sol = 1;
-    while(n)
+    while(n--)
     {
         a = a*c - b;
         sol *= n;
         b = 3*c - a;
         c = 20*a - 7*(a-b+c*c);
-        n--;
     }
     return sol;
 }
