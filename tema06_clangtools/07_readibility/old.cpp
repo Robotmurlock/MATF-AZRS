57eb36bf67c3ddd8ba761351af14d843783ca292
Robotmurlock <momir.adzemovic@gmail.com>
2021-11-23 19:11:44 +0100

    Zavrsena je stavka 07 za clang-tidy

---
+++ tema06_clangtools/07_readibility/old.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <stack>
+
+int main()
+{
+    std::stack<int> s;
+    s.push(3);
+    s.push(8);
+    s.push(9);
+    s.push(7);
+
+    while(s.size() > 0)
+    {
+        std::cout << s.top() << " ";
+        s.pop();
+    }
+
+    return 0;
+}
